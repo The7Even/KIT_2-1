92e47feeed0ffd3efa9968ff0f0bbc54ab030974
The7Even <161558340+The7Even@users.noreply.github.com>
2026-03-24 14:17:12 +0900

    Create Q4+. pointer usage overload ver.cpp

---
+++ C++ /3rd week/assignment/Q4+. pointer usage overload ver.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+void MaxMin(int &a, int &b)
+{
+	int temp;
+	if (a < b)
+	{
+		temp = a;
+		a = b;
+		b = temp;
+	}
+}
+
+void MaxMin(int *a, int *b)
+{
+	int temp;
+	if (*a < *b)
+	{
+		temp = *a;
+		*a = *b;
+		*b = temp;
+	}
+}
+
+int main()
+{
+	int max, min;
+	cout << "Input 2 Numbers. : ";
+	cin >> max >> min;
+
+	MaxMin(max, min);
+  MaxMin(&max, &min);
+
+	cout << "Max : " << max << endl;
+	cout << "Min : " << min << endl;
+}
