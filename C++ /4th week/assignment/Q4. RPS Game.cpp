4d0568b18688d5b5db19bfa9f0a67b96c51b4f43
The7Even <161558340+The7Even@users.noreply.github.com>
2026-03-30 12:08:52 +0900

    new idea

--- C++ /4th week/assignment/Q4. RPS Game.cpp
+++ C++ /4th week/assignment/Q4. RPS Game.cpp
@@ -22,6 +22,9 @@ int main()
 			continue;
 		}
 		// 가위바위보 승패여부 0 < 1 < 2 < 0 순서, 클수록 좋지만, 2는 0에게 패배.
+		// 승패 판독에서 새로운 방법이 있었는데..
+		// ((com + 1) % 3 == user)에서 True일경우 user 승, 같을경우는 무승부, 그 외에는 com 승. 밑의 조건보다 훨씬 간결함
+		// if ((comInput + 1) % 3 == playerInput)으로 28번줄을 대체하여도 가능함. 대신 30,31번과 37,38번줄 맞바꾸어야함.
 		if ((playerInput == 2 && comInput == 0) || (playerInput < comInput && playerInput != 2))
 		{
 			cout << "당신이 졌습니다." << endl;
