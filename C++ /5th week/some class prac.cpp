843d184dbe3e85debe157b07a3a23ae95df52eb5
The7Even <161558340+The7Even@users.noreply.github.com>
2026-03-31 12:35:11 +0900

    Update some class prac.cpp

--- C++ /5th week/some class prac.cpp
+++ C++ /5th week/some class prac.cpp
@@ -12,6 +12,7 @@ public:
 	void setXY(int x, int y) { x_ = x; y_ = y; }
 	int getX() { return x_; }
 	int getY() { return y_; }
+	Point moveXY(int x, int y) { x_ += x; y_ += y; return (*this); } // this 포인터 사용법
 };
 
 int GetsumX(Point ary[], int len)
@@ -36,6 +37,8 @@ int main() {
 	Point ptlist[5] = { Point(3, 5) , Point(2,6), Point(8), Point(6,7), Point(8,2)};
 	for (int i = 0; i < 5; i++)
 		ptlist[i].print();
+
+	ptlist[0].moveXY(1,2).moveXY(4,5); // 이렇게 해도 가능함. this 포인터 return해주면 가능함.
 
 	Point sum(GetsumX(ptlist, 5), GetsumY(ptlist, 5));
 	sum.print();
