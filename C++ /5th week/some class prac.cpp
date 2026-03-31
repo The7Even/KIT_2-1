0ddef1b01db694d5293856afa0397af72b47b675
The7Even <161558340+The7Even@users.noreply.github.com>
2026-03-31 12:05:06 +0900

    Create some class prac.cpp

---
+++ C++ /5th week/some class prac.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+class Point {
+private:
+	int x_, y_;
+public:
+	Point(int x, int y) : x_(x), y_(y) {}
+	Point(int x) : x_(x), y_(0) {}
+	Point() : x_(0), y_(0) {}
+	void print() { cout << "(" << x_ << ", " << y_ << ")" << endl; }
+	void setXY(int x, int y) { x_ = x; y_ = y; }
+	int getX() { return x_; }
+	int getY() { return y_; }
+};
+
+int GetsumX(Point ary[], int len)
+{
+	int dump = 0;
+	for (int i = 0; i < len; i++)
+		dump += ary[i].getX();
+
+	return dump;
+}
+
+int GetsumY(Point ary[], int len)
+{
+	int dump = 0;
+	for (int i = 0; i < len; i++)
+		dump += ary[i].getY();
+
+	return dump;
+}
+
+int main() {
+	Point ptlist[5] = { Point(3, 5) , Point(2,6), Point(8), Point(6,7), Point(8,2)};
+	for (int i = 0; i < 5; i++)
+		ptlist[i].print();
+
+	Point sum(GetsumX(ptlist, 5), GetsumY(ptlist, 5));
+	sum.print();
+}
