2dbb670282228f50c2041cfb552f7d65644785f5
The7Even <161558340+The7Even@users.noreply.github.com>
2026-05-02 09:54:41 +0900

    Create Q3. Book - Paper, Ebook.cpp

---
+++ C++ /9th week/Q3. Book - Paper, Ebook.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+class Book {
+protected:
+	string title_;
+	string author_;
+	int price_;
+public:
+	Book(string t, string a, int p) : title_(t), author_(a), price_(p) {}
+	void displayInfo() { cout << ">>> 도서명 : " << title_ << " | 저자 : " << author_ << " | 유형 : "; }
+	int GetPrice() { return price_; }
+};
+
+class PaperBook : public Book {
+protected:
+	int weight_;
+public:
+	PaperBook(string t, string a, int p, int w) : Book(t,a,p), weight_(w) {}
+	int GetPrice() { return Book::GetPrice() + 2500; }
+	void displayInfo() { Book::displayInfo(); cout << "종이책 (무게 : " << weight_ << "g)" << endl; }
+};
+
+class Ebook : public Book {
+protected:
+	string format_;
+public:
+	Ebook(string t, string a, int p, string f) : Book(t,a,p), format_(f) {}
+	int GetPrice() { return Book::GetPrice(); }
+	void displayInfo() { Book::displayInfo(); cout << "전자책 (포맷 : " << format_ << ")" << endl; }
+};
+
+int main()
+{
+	cout << "=== 도서 관리 시스템 동작===" << endl;
+
+	// 1. 종이책 생성 및 테스트
+	PaperBook p1("C++ 프로그래밍", "황준하", 25000, 800);
+	p1.displayInfo();
+	cout << ">> 배송비 포함 결제 금액 : " << p1.GetPrice() << " 원" << endl;
+
+	cout << "----------------------------------------------" << endl;
+
+	// 2. 전자책 생성 및 테스트
+	Ebook e1("파이썬 프로그래밍", "김코드", 18000, "pdf");
+	e1.displayInfo();
+	cout << ">> 결제 금액 : " << e1.GetPrice() << " 원" << endl;
+}
